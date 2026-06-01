1cbe4a16c8a93975bfc2e7b9b53da8e8f2784c6b
moumadjad <moumadjad.a@gmail.com>
2026-06-01 19:21:16 +0000

    add the login page

---
+++ src/pages/Auth/Login.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = () => {console.log("Login")};
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+            <Card className="w-full max-w-md shadow-xl">
+                <CardHeader className="text-center">
+                    <CardTitle className="text-2xl">Sign In</CardTitle>
+                </CardHeader>
+
+                <CardContent>
+                    <form onSubmit={handleSubmit} className="space-y-4">
+                        <div className="space-y-2">
+                            <Label>Email</Label>
+                            <Input
+                                type="email"
+                                placeholder="example@gmail.com"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <div className="space-y-2">
+                            <Label>Password</Label>
+                            <Input
+                                type="password"
+                                placeholder="********"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <Button type="submit" className="w-full">
+                            Sign In
+                        </Button>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
